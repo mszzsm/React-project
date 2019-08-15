a219190eb91380248ba135d37f15bf77b173b17d
Michał Sztefanica <mikeshtefanitsa@gmail.com>
2019-08-15 22:59:04 +0200

    Grid added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 const App = () => {
   return (
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="https://picsum.photos/200/300/?blur"  alt="asd"/>
+      </header>
+
+      <nav className="nav"> 
+       
+          <div>
+            Profile
+          </div>
+          <div>
+            Message
+          </div>
+          <div>
+            Main Content
+          </div>
+        
+      </nav>
+
+    <div className="content">
+      </div>
+
+      <footer> 
+
+      </footer>
+    </div>
     
   )
 
